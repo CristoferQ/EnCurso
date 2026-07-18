3601443a953e91cafbff120320b4a50478bbf381
CristoferQ <ceql@live.cl>
2026-07-18 00:11:46 -0400

    Initial project version

---
+++ src/main/java/com/encurso/exception/CourseAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.encurso.exception;
+
+public class CourseAlreadyExistsException extends RuntimeException {
+    public CourseAlreadyExistsException(String message) {
+        super(message);
+    }
+}
